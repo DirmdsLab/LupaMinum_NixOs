8b77abe9664d04fc33837b916c8ddea3eaabf42e
DirmdsLab <141147147+DirmdsLab@users.noreply.github.com>
2026-02-20 19:36:10 +0700

    rx 580

--- modules/virt-manager/vm.nix
+++ modules/virt-manager/vm.nix
@@ -5,35 +5,13 @@
 
   users.groups.libvirtd.members = [ "tutturuu" ];
 
-  virtualisation.libvirtd.enable = true;
-
-  virtualisation.spiceUSBRedirection.enable = true;
-
-  # --- VFIO GPU passthrough ---
-  boot.kernelModules = [ "vfio_pci" "vfio_iommu_type1" "vfio" "vendor_reset" ];
-
-  boot.extraModprobeConfig = ''
-    options vfio-pci ids=1002:6fdf,1002:aaf0
-  '';
-  
-}
-
-{ config, lib, pkgs, ... }:
-
-{
-  programs.virt-manager.enable = true;
-
-  users.groups.libvirtd.members = [ "tutturuu" ];
-
   virtualisation.libvirtd = {
     enable = true;
 
     qemu = {
-      swtpm.enable = true;     # TPM 2.0 emulator
-      ovmf = {
-        enable = true;
-        packages = [ pkgs.OVMFFull ]; # Secure Boot support
-      };
+
+      swtpm.enable = true;     
+    
     };
   };
 
